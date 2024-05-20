3b628f0b2bd1360f00ee9a523d371cea88f9a34c
khanjan-28 <parmarkhanjan398@gmail.com>
2024-05-21 00:49:11 +0530

    Add files via upload

---
+++ Area.java
@@ -0,0 +1,16 @@
+package Project2;
+
+public class Area {
+
+	private float l,b;
+
+	public float getArea() {
+		return l*b;
+		
+	}
+
+	public void setDim(float l,float b) {
+		this.l = l;
+		this.b = b;
+	}	
+}
---
+++ Rectangle.java
@@ -0,0 +1,14 @@
+package Project2;
+
+public class Rectangle {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Area a=new Area();
+		a.setDim(20.10f,22.22f);
+		System.out.println("Area of Rectangle-->"+a.getArea());
+		
+	}
+
+}
